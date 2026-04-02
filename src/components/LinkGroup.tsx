01f66d95ab5f8432c0ee6550d04badd1abf63f51
fabelfaye <examonlinehub@hotmail.com>
2026-04-02 09:19:41 +0800

    Update Youtube title text in LinkGroup component

--- src/components/LinkGroup.tsx
+++ src/components/LinkGroup.tsx
@@ -85,7 +85,7 @@ export const LinkGroup: React.FC<LinkGroupProps> = ({
                 "font-semibold text-foreground/80 whitespace-nowrap",
                 (title === 'Youtube' || title === '小红书' || title === '微博' || title === '抖音' || title === 'B站') ? "text-base" : "text-xl"
               )}>
-                {title === 'Youtube' && '《破爱》播放平台'}
+                {title === 'Youtube' && '《破爱》内外网播放平台'}
                 {title === '小红书' && '小红书~《破爱》周边物料'}
                 {title === '微博' && '微博~《破爱》官方动态'}
                 {title === '抖音' && '抖音~《破爱》短视频'}
