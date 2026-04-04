5f19e4a334b8539dbfa2717fda52935aa3366e78
fabelfaye <examonlinehub@hotmail.com>
2026-04-04 22:49:42 +0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -27,7 +27,7 @@ const youtubeLinks: LinkItemData[] = [
   { title: '【外网】Fabel油管平台', url: 'https://www.youtube.com/@FabelEntertainment', updateDate: '第一行' },
   { title: '【内网】EP1~ 葵葵岛种小芒果', url: 'https://www.bilibili.com/video/BV1MSXMBiEV5', updateDate: '第二行' },
   { title: '【内网】EP1~ 真的肥常无语', url: 'https://www.bilibili.com/video/BV1jpXLBMEUy', updateDate: '第二行' },
-  { title: '【中翻】EP2~ 葵葵岛种小芒果 待更新', url: 'https://www.bilibili.com', updateDate: '第二行' },
+  { title: '【中翻】EP2~ 泰甜小剧院', url: 'https://www.bilibili.com/video/BV1L6DuByEb5', updateDate: '第二行' },
   { title: '【中翻】EP2~ 真的肥常无语 待更新', url: 'https://www.bilibili.com', updateDate: '第二行' },
 ];
 
