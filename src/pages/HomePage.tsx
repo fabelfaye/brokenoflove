c6a894579787729b7efc1e0ebf397b74e6f08da1
fabelfaye <examonlinehub@hotmail.com>
2026-04-19 10:47:42 +0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -56,6 +56,7 @@ const youtubeLinks: LinkItemData[] = [
 ];
 
 const weiboLinks: LinkItemData[] = [
+  { title: 'EP4预告视频', url: 'https://weibo.com/7985880343/QBoRHD3bb', updateDate: '预告' },
   { title: 'EP3预告视频', url: 'https://weibo.com/7985880343/QAl4kdai6', updateDate: '预告' },
   { title: 'EP2预告视频', url: 'https://weibo.com/7985880343/QzhafeXAU', updateDate: '预告' },
   { title: 'EP2预告图片', url: 'https://weibo.com/7985880343/QyD7hyg2B', updateDate: '预告' },
